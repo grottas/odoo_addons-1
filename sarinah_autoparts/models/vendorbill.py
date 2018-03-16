01a299ae0bfa6a2b6aa53658d02438af2ebd10f4
Budi Hartono <budi.hoo@gmail.com>
2018-03-17 03:17:14 +0700

    Fix: E-Faktur should be in model account.invoice
    Fix: E-Faktur should be in model account.invoice. Previously it was in model purchase.order.

--- sarinah_autoparts/models/vendorbill.py
+++ sarinah_autoparts/models/vendorbill.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, api
 
 class EfakturPurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = 'account.invoice'
 
     vendor_efaktur = fields.Char("No. E-Faktur")
