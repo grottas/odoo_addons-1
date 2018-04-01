8237cbf1b2cc3343d1e4112b57219bd1fb695692
Budi Hartono <budi.hoo@gmail.com>
2018-04-02 04:01:19 +0700

    Version 1.1 - Add Giro
    Add giro number to account.payment and add search filter Current Month and Previous Month to Invoice Tree/Search View.

--- sarinah_autoparts/__manifest__.py
+++ sarinah_autoparts/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Sales',
-    'version': '1.0',
+    'version': '1.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale'],
@@ -33,6 +33,7 @@
         'views/product.xml',
         'views/vendorbill.xml',
         'views/customerinvoice.xml',
+        'views/searchfilters.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- sarinah_autoparts/models/vendorbill.py
+++ sarinah_autoparts/models/vendorbill.py
@@ -4,3 +4,9 @@ class EfakturPurchaseOrder(models.Model):
     _inherit = 'account.invoice'
 
     vendor_efaktur = fields.Char("No. E-Faktur")
+
+class GiroPayment(models.Model):
+    _inherit = 'account.payment'
+
+    giro_number = fields.Char("No. Giro")
+    journal_name = fields.Char(string="Journal Name", related="journal_id.name")
